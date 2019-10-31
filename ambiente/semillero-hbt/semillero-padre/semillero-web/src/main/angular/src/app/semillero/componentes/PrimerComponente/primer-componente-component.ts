40fdc893dec2a04de36618e8ed897787a5f4fab5
Sebastian <jsalzatel@uqvirtual.edu.co>
2019-10-30 23:21:07 -0500

    correciones

--- ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/PrimerComponente/primer-componente-component.ts
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/PrimerComponente/primer-componente-component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * @description Componente primer comentario, el cual contiene el nombre, la ciudad y el 
+ * repositorio de Sebastian Alzate
+ * 
+ * @author Sebastian Alzate <jsalzatel@uqvirtual.edu.co>
+ */
 @Component({
     selector: 'primer-componente',
     templateUrl: './primer-componente-component.html'
 
 })
 
+/**
+ * exportando la clase
+ */
 export class PrimerComponenteComponent{
 
 }
